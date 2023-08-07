27c2e2f83725a1554dc7650ce9fc8e3ee5610ff9
woohaen88 <woohaen88@gmail.com>
2023-08-07 11:08:37 +0900

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Text} from "@chakra-ui/react";
+
+function App() {
+    return <div>
+        <Text fontSize={"xl"} color={"red.600"}>It Works!!</Text>
+    </div>;
+}
+
+export default App;
