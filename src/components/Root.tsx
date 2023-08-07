53ea039acc5f48de0214930e23e414b114fa6e93
woohaen88 <woohaen88@gmail.com>
2023-08-07 14:44:24 +0900

    Feat: Header 추가

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,5 +1,18 @@
 import {Outlet} from "react-router-dom";
+import {Box, Button, HStack} from "@chakra-ui/react";
+import {FaAirbnb} from "react-icons/fa";
 
 export default function Root() {
-    return <h1>im root <Outlet/></h1>;
+    return <Box>
+        <HStack justifyContent={"space-between"} py={"5"} px={"10 "}>
+            <Box color={"red.500"}>
+                <FaAirbnb size={"38"}/>
+            </Box>
+            <HStack spacing={"2"}>
+                <Button>Log In</Button>
+                <Button colorScheme={"red"}>Sign Up</Button>
+            </HStack>
+        </HStack>
+            <Outlet/>
+        </Box>;
 }
